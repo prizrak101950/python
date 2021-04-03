c4b660dbefd87fb8f3068da9ac81171b4443964a
prizrak101950 <78309814+prizrak101950@users.noreply.github.com>
2021-04-03 12:42:32 +0300

    Add while loop

--- level2.py
+++ level2.py
@@ -1,13 +1,16 @@
-a = int(input("введи первое число "))
-b = int(input("введи второе число "))
-c = int(input("введи третье число "))
+count = 1
+while count <= 3:
 
-m = a
-if (a > b) and (a > c):
-    m = a
-elif b > c:
-    m = b
-else:
-    m = c
-print(m)
+    a = int(input("введи первое число "))
+    b = int(input("введи второе число "))
+    c = int(input("введи третье число "))
 
+    mx = a
+    if (a > b) and (a > c):
+        mx = a
+    elif b > c:
+        mx = b
+    else:
+        mx = c
+    print(mx)
+    count += 1
